25c5d322319cd85aaa213e4b42bfda8f85d6c4d9
Diego Silva <odiegosilvadev@gmail.com>
2023-08-23 15:53:36 -0300

    update js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # New-Project-JS
  
+fonte imagens 
+
+https://media.giphy.com/media/DrJm6F9poo4aA/giphy.gif
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,23 @@
-alert('cú')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+                      
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom Dia!
+        img.src = './imagens/manha-img.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa tarde!
+        img.src = './imagens/tarde-img.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // Boa Noite!
+        img.src = './imagens/noite-img.png'
+        document.body.style.background = '#515154'
+    }
+}
+
+
